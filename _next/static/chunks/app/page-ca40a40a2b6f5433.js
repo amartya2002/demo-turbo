25c0dee52086b513308071463dd648d05a9ca825
Amartya Sinha <amartua2002@gmail.com>
2023-12-22 12:09:53 +0530

    Updates

---
+++ _next/static/chunks/app/page-ca40a40a2b6f5433.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{312:function(){}},function(n){n.O(0,[556,381,744],function(){return n(n.s=312)}),_N_E=n.O()}]);
---
+++ _next/static/pKMQFeThcRtOsuStAL-pt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-fa4996fbd5da8517.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
